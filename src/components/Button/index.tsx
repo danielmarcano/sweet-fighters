1a3bfd066063e03e8b5b4e4fa675a7f03a964e85
Daniel Marcano <danielmarcanodev@gmail.com>
2023-10-24 23:01:50 +0200

    feat: create Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,36 @@
+import type { ReactNode } from "react";
+import type { CSSColor } from "../../utils/obtainCSSColor";
+import { obtainCSSColor } from "../../utils/obtainCSSColor";
+import styles from "./index.module.css";
+
+type ButtonProps = Partial<Omit<HTMLButtonElement, "children">> & {
+  type: "button" | "submit";
+  color: CSSColor;
+  children?: ReactNode;
+  disabled?: boolean;
+  onClick: () => void;
+  className?: string;
+};
+
+export function Button({
+  type = "button",
+  color,
+  children,
+  disabled,
+  onClick,
+  className,
+}: ButtonProps) {
+  const CSSColor = obtainCSSColor(color);
+
+  return (
+    <button
+      type={type}
+      className={`${className} ${styles.container}`}
+      style={{ color: CSSColor, borderColor: CSSColor }}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}

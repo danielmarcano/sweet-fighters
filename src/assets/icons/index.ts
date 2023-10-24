b303672a8ed5326fd9df46f222c7109ec1474ccd
Daniel Marcano <danielmarcanodev@gmail.com>
2023-10-25 01:18:55 +0200

    feat: add vite-plugin-svgr package to handle SVG imports, and first SVG icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite-plugin-svgr/client" />
+// This is a reference directive needed so that TypeScript does
+// not complain about the ?react suffix
+// More info here: https://www.npmjs.com/package/vite-plugin-svgr#usage
+export { default as SoundMaxIcon } from "./sound_max.svg?react";
+export { default as SoundMinIcon } from "./sound_mute.svg?react";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      esbuildOptions: {
+        jsx: "automatic",
+      },
+      include: "**/*.svg?react",
+      svgrOptions: {
+        // More info here: https://react-svgr.com/docs/options/#svg-props
+        svgProps: {
+          // It prevents screen-readers to attempt to read SVGs
+          "aria-hidden": "true",
+        },
+        // Colors taken from colors.css
+        // More info here: https://react-svgr.com/docs/options/#replace-attribute-value
+        replaceAttrValues: {
+          "#FFD6E0": "currentColor",
+          "#C1FBA4": "currentColor",
+          "#7BF1A8": "currentColor",
+          "#FFEF9F": "currentColor",
+          "#4AD5FF": "currentColor",
+          "#0C0F0A": "currentColor",
+        },
+      },
+    }),
+  ],
+});

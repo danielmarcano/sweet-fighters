9548837bcd749dce4698685d6406a2b0b36b1497
Daniel Marcano <danielmarcanodev@gmail.com>
2023-10-25 00:24:02 +0200

    feat: add provisional src/locale/en.ts file

---
+++ src/locale/en.ts
@@ -0,0 +1,13 @@
+/*
+ * Provisional translations file.
+ *
+ * Once we add a translations library, we can turn this file into "en.json",
+ * and place the "locale" directory within the "public" one
+ */
+
+export const translations = {
+  "screens.main.musicButton.turnOffLabel": "Turn sound off",
+  "screens.main.musicButton.turnOnLabel": "Turn sound on",
+  "screens.main.title": "Sweet Huggers",
+  "screens.main.startGameLabel": "Start Game",
+};

41eed6dd25acb28c933b4bb5fe062ae6ecbf310d
Daniel Marcano <danielmarcanodev@gmail.com>
2023-10-25 13:37:32 +0200

    feat: include useAudioPlayer within MusicButton, and place it within AudioPlayer directory

---
+++ src/providers/AudioPlayer/MusicButton/index.tsx
@@ -0,0 +1,42 @@
+import { translations } from "../../../locale/en";
+import { Button } from "../../../components/Button";
+import { CSSColor } from "../../../utils/obtainCSSColor";
+import { SoundMaxIcon, SoundMinIcon } from "../../../assets/icons";
+import styles from "./index.module.css";
+import { useAudioPlayer } from "../useAudioPlayer";
+
+type MusicButtonProps = {
+  className?: string;
+};
+
+export function MusicButton({ className }: MusicButtonProps) {
+  const {
+    playAudio,
+    pauseAudio,
+    audioElementProps: { isPlaying },
+  } = useAudioPlayer();
+
+  return (
+    <>
+      <Button
+        className={`${className} ${styles.container}`}
+        color={isPlaying ? CSSColor.GRASS : CSSColor.ROSE}
+        type="button"
+        ariaLabel={
+          isPlaying
+            ? translations["screens.main.musicButton.turnOffLabel"]
+            : translations["screens.main.musicButton.turnOnLabel"]
+        }
+        onClick={() => {
+          if (isPlaying) {
+            return pauseAudio();
+          }
+
+          playAudio();
+        }}
+      >
+        {isPlaying ? <SoundMaxIcon /> : <SoundMinIcon />}
+      </Button>
+    </>
+  );
+}
